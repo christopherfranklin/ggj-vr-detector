aac28cea3a84c505aa650dd83ca483967625e7e0
Chris Franklin <christopher.alan.franklin@gmail.com>
2018-01-27 12:47:05 -0500

    Add light source for objects

---
+++ Assets/Scripts/DetectorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorController : MonoBehaviour {
+
+	public GameObject m_pulse;
+
+	private OVRGrabbable m_grabbable;
+
+	// Use this for initialization
+	void Start () {
+		m_grabbable = GetComponent<OVRGrabbable> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (m_grabbable.isGrabbed) {
+			// Send out pulse
+		}
+	}
+}
